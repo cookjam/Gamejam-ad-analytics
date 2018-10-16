558a8c7d3c25ffa90256bc3a1b81c6874c899698
turtur <yjko@cookapps.com>
2018-10-16 19:12:17 +0900

    firebase analytics import

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Firebase.Analytics.FirebaseAnalytics
+		.LogEvent(Firebase.Analytics.FirebaseAnalytics.EventLogin);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
